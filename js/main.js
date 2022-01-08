b38dcd1172ecbb9a1532946203f1e7c844aa2677
Adeleke Israel <izzyfresher@gmail.com>
2022-01-08 03:15:18 +0100

    commented flutter payment integration

--- js/main.js
+++ js/main.js
@@ -5,61 +5,60 @@ burger.addEventListener("click", function () {
   navMenu.classList.toggle("open");
 });
 
+var partnersForm = document.getElementById("partnersForm");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  var name = document.getElementById("contact-name").value;
+  var email = document.getElementById("contact-email").value;
+  var phone = document.getElementById("contact-phone").value;
+  var message = document.getElementById("contact-message").value;
 
+  fetch("https://tedxfuta.herokuapp.com/partners_form", {
+    method: "POST",
+    body: JSON.stringify({
+      first_name: first_name,
+      email: email,
+      phone: phone,
+      message: message,
+    }),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+      "X-CSRFToken":
+        "iAiX7PfFfpMhPcbz9712GdBnk08A9oljxChVjad8Ku5OxjdgQjpqwJHXEcoM0fw3",
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response.data);
+    });
 
+  form.reset();
+});
 
+//Paymet integration for Flutterwave
 
-var partnersForm = document.getElementById('partnersForm')
-form.addEventListener('submit', function(e) {
-	e.preventDefault();
-	var name = document.getElementById("contact-name").value
-	var email = document.getElementById("contact-email").value
-	var phone = document.getElementById("contact-phone").value
-	var message = document.getElementById("contact-message").value
+// const payNow = (e) => {
+//   e.preventDefault();
 
+//   FlutterwaveCheckout({
+//     public_key: "",
+//     tx_ref: "tedxfuta_" + Math.floor(Math.random() * 10000000 + 1),
+//     amount: document.getElementById("amount").value,
+//     currency: "NGN",
+//     customer: {
+//       email: document.getElementById("email").value,
+//       phonenumber: document.getElementById("phoneNumber").value,
+//       name: document.getElementById("fullname").value,
+//     },
+//     callback: function (data) {
+//       // console.log(data)
 
-	fetch( 'https://tedxfuta.herokuapp.com/partners_form', {
-		method: 'POST',
-		body: JSON.stringify({
-			first_name: first_name,
-			email: email,
-			phone: phone,
-			message: message
-		}), 
-		headers: {
-			"Content-Type": "application/json; charset=UTF-8",
-      "X-CSRFToken": "iAiX7PfFfpMhPcbz9712GdBnk08A9oljxChVjad8Ku5OxjdgQjpqwJHXEcoM0fw3"
-			}
-	},
-	)
-		.then( response => response.json() )
-		.then( response => {
-				console.log(response.data)
-		});
-
-
-		form.reset();
-
-
-
-})
-
-
-const payNow = (e) => {
-	e.preventDefault();
-
-	FlutterwaveCheckout({
-		public_key: "",
-		tx_ref: "tedxfuta_" + Math.floor((Math.random() * 10000000) + 1),
-		amount: document.getElementById("amount").value,
-		currency: "NGN",
-		customer: {
-			email: document.getElementById("email").value,
-			phonenumber: document.getElementById("phoneNumber").value,
-			name: document.getElementById("fullname").value
-		},
-		callback: function(data) {
-			console.log(data)
-		}
-	})
-}
+//       const refrence = data.tx_ref;
+//       alert("Payment was successfully Completed" + refrence);
+//     },
+//     customization: {
+//       title: "TEdxFUTA Ticketing Payments",
+//       description: "Fullterwave paym,ent integration for TedxFuta",
+//     },
+//   });
+// };
