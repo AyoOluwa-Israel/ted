d95e8fc10f8953fb5eb4b2580246cdbc4e28ab1b
Adeleke Israel <izzyfresher@gmail.com>
2022-01-08 14:48:01 +0100

    ediited code

--- js/main.js
+++ js/main.js
@@ -6,54 +6,57 @@ burger.addEventListener("click", function () {
 });
 
 const sponsorsForm = document.getElementById("sponsorsForm");
-sponsorsForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const first_name = document.getElementById("first_name").value;
-  const last_name = document.getElementById("last_name").value;
-  const email = document.getElementById("email").value;
-  const phone = document.getElementById("phone").value;
-  // var message = document.getElementById("message").value;
-  var business_name = document.getElementById("business_name").value;
-
-  fetch("https://tedxfuta.herokuapp.com/partners_form", {
-    method: "POST",
-    body: JSON.stringify({
-      first_name: first_name,
-      last_name: last_name,
-      email: email,
-      phone: phone,
-      business_name: business_name,
-    }),
-    headers: {
-      "Content-Type": "application/json; charset=UTF-8",
-      "X-CSRFToken":
-        "iAiX7PfFfpMhPcbz9712GdBnk08A9oljxChVjad8Ku5OxjdgQjpqwJHXEcoM0fw3",
-    },
-  })
-    .then((response) => response.json())
-    .then((response) => {
-      console.log(response.data);
-      alert("You have successfully submitted the form");
+if (sponsorsForm){
+  sponsorsForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const first_name = document.getElementById("first_name").value;
+    const last_name = document.getElementById("last_name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    // var message = document.getElementById("message").value;
+    const business_name = document.getElementById("business_name").value;
+  
+    fetch("https://tedxfuta.herokuapp.com/partners_form", {
+      method: "POST",
+      body: JSON.stringify({
+        first_name: first_name,
+        last_name: last_name,
+        email: email,
+        phone: phone,
+        business_name: business_name,
+      }),
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+        "X-CSRFToken":
+          "iAiX7PfFfpMhPcbz9712GdBnk08A9oljxChVjad8Ku5OxjdgQjpqwJHXEcoM0fw3",
+      },
     })
-    .catch((error) => {
-      console.log(error);
-      alert("Invalid submission. Please Check your Fields");
-    });
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.data);
+        alert("You have successfully submitted the form");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Invalid submission. Please Check your Fields");
+      });
+  
+    sponsorsForm.reset();
+  });
+}
 
-  sponsorsForm.reset();
-});
 
 const ticketForm = document.getElementById("ticketForm");
 
 ticketForm.addEventListener("submit", (e) => {
 	e.preventDefault();
-  const full_name = document.getElementById("full_name");
-  const email = document.getElementById("email");
-  const phone = document.getElementById("phone");
-  const occupation = document.getElementById("occupation");
-  const no_of_ticket = document.getElementById("no_of_ticket");
+  const full_name = document.getElementById("full_name").value
+  const email = document.getElementById("email").value
+  const phone = document.getElementById("phone").value
+  const occupation = document.getElementById("occupation").value
+  const no_of_ticket = document.getElementById("no_of_ticket").value
 
-  const amount= 2030;
+  const amount= 2030 * no_of_ticket;
 
   fetch("https://tedxfuta.herokuapp.com/ticket_form", {
     method: "POST",
@@ -62,7 +65,7 @@ ticketForm.addEventListener("submit", (e) => {
       email: email,
       phone: phone,
       occupation: occupation,
-      no_of_ticket: no_of_ticket,
+      no_of_ticket: 1,
     }),
     headers: {
       "Content-Type": "application/json; charset=UTF-8",
@@ -89,9 +92,9 @@ ticketForm.addEventListener("submit", (e) => {
       metadata: {
           custom_fields: [
               {
-                  display_name: fullname,
-                  variable_name: fullname,
-                  value: phone_number
+                  display_name: full_name,
+                  variable_name: full_name,
+                  value: phone
               }
           ]
       },
