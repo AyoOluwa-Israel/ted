b94d2f77a02d4c5cbe6b175459353c87ee8cee14
Daniel130me <69328757+Daniel130me@users.noreply.github.com>
2022-01-11 13:26:42 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ if (ticketForm) {
     const amount = 2030 * no_of_ticket;
 
     var handler = PaystackPop.setup({
-      key: "pk_test_cf31c9ce121a70cd11c6fee1689c80e3b3ef42be",
+      key: "pk_live_c669857beaa96ab7bd7619f4664a881d893669f4",
       email: email,
       amount: amount * 100,
       ref: "TEDx" + Date.now(),
